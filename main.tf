5acf3c43e65d6a04d90524753ec3c104a47a6ab8
Mone19 <fuocoangelos@yahoo.de>
2024-09-01 14:35:36 +0200

    testing

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ resource "kubernetes_secret" "tls_cert" {
   }
 
   data = {
-    "tls.crt" = base64decode(var.tls_cert)
-    "tls.key" = base64decode(var.tls_key)
+    "tls.crt" = file(var.tls_cert)
+    "tls.key" = file(var.tls_key)
   }
 }
 
