fdfa721784e2e0673d9807226a3e25e7f60fad5e
Mone19 <fuocoangelos@yahoo.de>
2024-09-01 11:17:37 +0200

    testing

--- main.tf
+++ main.tf
@@ -122,3 +122,12 @@ data "azurerm_kubernetes_cluster" "aks" {
   name                = azurerm_kubernetes_cluster.aks.name
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+# Output Jenkins Pipline
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
