dc38d1dcc885d930445d90ad25b09dc580c164cd
Mone19 <fuocoangelos@yahoo.de>
2024-09-03 01:40:48 +0200

    deploy

--- main.tf
+++ main.tf
@@ -59,13 +59,24 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "allow"
+    name                       = "http"
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = 8080
+    destination_port_range     = 80
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "https"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 443
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
